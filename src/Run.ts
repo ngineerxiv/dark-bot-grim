205811914617b0c45a27a6666ed11f74e126e199
Arata Tanaka <tarata43@yahoo.co.jp>
2019-12-14 01:25:22 +0900

    Fix port's env name

--- src/Run.ts
+++ src/Run.ts
@@ -3,7 +3,7 @@ import { init as slackInit } from "./Slack";
 function main(): void {
   const slackBotToken = process.env.SLACK_BOT_TOKEN;
   const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
-  const port = process.env.SLACK_EVENT_PORT || "8080";
+  const port = process.env.PORT || "8080";
 
   slackInit(slackBotToken, slackSigningSecret, port).catch(e => {
     console.error(`Slack Error`);
