24ee43dfce54e81fae36be35159c5c0589290c29
Arata Tanaka <tarata43@yahoo.co.jp>
2019-12-14 02:00:22 +0900

    Fix bot name

--- src/Reaction.ts
+++ src/Reaction.ts
@@ -6,19 +6,19 @@ type BotReaction = (
 type Help = string;
 
 export const MentionedReactions: Array<[RegExp, BotReaction, Help?]> = (() => {
-  const botName = "@dark"; // TODO get from slack app
+  const botName = "@grim"; // TODO get from slack app
   const r: Array<[RegExp, BotReaction, Help?]> = [
     [
       /PING/i,
       async send => {
         send("PONG");
       },
-      `${botName} ping`
+      `${botName} ping - pong`
     ]
   ];
 
   r.push([
-    /help$/i,
+    /HELP$/i,
     async messageSend => {
       const helps = r.map(v => v[2]).filter(v => v !== undefined);
       const message = "\n" + helps.join("\n");
