6f8187cebed2c60691e3f7042dd20c3409d28062
Arata Tanaka <tarata43@yahoo.co.jp>
2020-02-27 13:01:34 +0900

    Add redis for save timeline message

--- src/Run.ts
+++ src/Run.ts
@@ -1,8 +1,13 @@
 import { init as slackInit } from './Slack';
 import { env, validateEnv } from './Env';
+import * as redis from 'redis';
 
 async function main(): Promise<void> {
   await validateEnv(env);
+  const redisClient = redis.createClient(env.redisUrl);
+  redisClient.on('error', err => {
+    console.error(err);
+  });
   await slackInit(env.slackBotToken, env.slackSigningSecret, env.port);
   console.log(`Slack Event Application started with port ${env.port}`);
 }
