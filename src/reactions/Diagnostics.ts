b650e4748cc7149c3a948dd745d26ee2d1faeabb
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2023-10-15 22:39:02 +0900

    Fix pong message image to AA

--- src/reactions/Diagnostics.ts
+++ src/reactions/Diagnostics.ts
@@ -1,12 +1,11 @@
 import { BotReaction } from '../Reaction';
-import { applyCacheBuster } from '../util/Url';
+import { HelloHello } from '../domains/B';
 
 export const reactions: Array<BotReaction> = [
   {
     pattern: /PING/i,
     reaction: async (send): Promise<void> => {
-      const p = applyCacheBuster('http://yamiga.waka.ru.com/images/ping.jpg');
-      send(p);
+      send(HelloHello);
     },
     help: (b: string): string => `${b} ping - ハローハロー`,
   },
